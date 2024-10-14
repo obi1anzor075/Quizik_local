b3edd0d752545add757ebfa4a6f9796f3db6abca
obi1anzor075 <shindin112@gmail.com>
2024-10-14 23:41:21 +0300

    update select mode
    working but not completed

---
+++ PresentationLayer/wwwroot/js/swiper.js
@@ -0,0 +1,14 @@
+﻿const swiper = new Swiper('.card-wrapper', {
+    loop: true,
+
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
